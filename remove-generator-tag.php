5cae5d9bb8b335bf00ead9776e8f0b217abbf137
Nils Windisch <nils@windisch.me>
2019-08-12 12:13:15 +0200

    INITIAL

---
+++ remove-generator-tag.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @package Remove WordPress Generator Tag
+ * @version 1.0
+ */
+/*
+Plugin Name: 
+Plugin URI: 
+Description: Remove WordPress Generator Tag
+Author: Nils Windisch
+Version: 1.0
+Author URI: 
+*/
+
+remove_action('wp_head', 'wp_generator');
